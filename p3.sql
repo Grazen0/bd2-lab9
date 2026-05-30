c560453421f9dd1845b858b93740cf5d16c12733
José Daniel Grayson <josedanielgrayson@proton.me>
2026-05-30 00:35:09 -0500

    feat(p3): finish p3

--- p3.sql
+++ p3.sql
@@ -1,8 +1,6 @@
-begin transaction;
+drop table if exists articles_full;
 
-drop table if exists articles;
-
-create table articles (
+create table articles_full (
   n int not null,
   id int primary key,
   title varchar not null,
@@ -15,7 +13,11 @@ create table articles (
   content text not null
 );
 
-\copy articles from 'large_news.csv' delimiter ',' csv header;
+\copy articles_full from 'large_news.csv' delimiter ',' csv header;
+
+drop table if exists articles;
+
+create table articles as select * from articles_full;
 
 alter table articles
 add column full_text tsvector generated always as (to_tsvector ('english', title || ' ' || content)) stored;
@@ -26,53 +28,29 @@ add column full_text_gin tsvector generated always as (to_tsvector ('english', t
 create index idx_full_text_gin on articles using gin (full_text_gin);
 
 explain analyze
-select
-  title, content
-from
-  articles
-where
-  full_text @@ to_tsquery('english', 'Man & Woman');
+select title, content
+from articles
+where full_text @@ to_tsquery('english', 'Justice | House');
 
 explain analyze
-select
-  title, content
-from
-  articles
-where
-  full_text_gin @@ to_tsquery('english', 'Man & Woman');
+select title, content
+from articles
+where full_text_gin @@ to_tsquery('english', 'Justice | House');
 
 explain analyze
-with q as (
-  select to_tsquery('english', 'Man & Woman') as query
-)
-select
-  title, content, ts_rank(full_text, q.query) as "rank"
-from
-  articles
-cross join
-  q
-where
-  full_text @@ q.query
-order by
-  "rank" desc
-limit
-  10;
+with q as ( select to_tsquery('english', 'Justice | House') as query)
+select title, content, ts_rank(full_text, q.query) as "rank"
+from articles
+cross join q
+where full_text @@ q.query
+order by "rank" desc
+limit 10000;
 
 explain analyze
-with q as (
-  select to_tsquery('english', 'Man & Woman') as query
-)
-select
-  title, content, ts_rank(full_text_gin, q.query) as "rank"
-from
-  articles
-cross join
-  q
-where
-  full_text_gin @@ q.query
-order by
-  "rank" desc
-limit
-  10;
-
-rollback;
+with q as ( select to_tsquery('english', 'Justice | House') as query)
+select title, content, ts_rank(full_text_gin, q.query) as "rank"
+from articles
+cross join q
+where full_text_gin @@ q.query
+order by "rank" desc
+limit 10000;
